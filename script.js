d693096fcce164062a89f8b9abec67c7b50fc525
jay sean <jaysean.developer@gmail.com>
2023-07-02 14:04:28 +0530

    rebase one

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let a  = 10
 console.log(a)
 
-let b = 1011222
+let b = 101
 
+<<<<<<< HEAD
 const prec = "pre chagne 1"
 const pre2 = "pre chagne 2 33"
 
 
 const name = "jay"
 const name2 = "rajesh"
+=======
+const test = "test branch"
+const test1 = "test branch second chang"
+>>>>>>> ca90e2d (rebase test 1)
